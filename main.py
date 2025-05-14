142ca4b3d984ca71deba1127959368b83a332ad4
dogwaffles <ldreillywillhight@students.everettcc.edu>
2025-05-14 13:39:15 +0000

    refactored: removed some unneccessaary variables, updated comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ mission_success = [True, False, True, True, True, True, False]
 
 
 #stores the number of missions as a variable, outputs the number
-#I use variables here to make the print() less complicated - easier to debug
+#I use variables here to make print() less complicated & easier to debug
 mission_num = len(mission_names) 
 print("\nThere were \033[0;34m" + str(mission_num) + "\033[0m total missions.")
 
@@ -19,21 +19,19 @@ print(f"Mission success rate was \033[0;34m{miss_suc_rate:.2f}\033[0m%.")
 
 #setting up some variables for use in creating output arrays
 year_index_list = []
-mission_years_filtered = []
 
 #uses enumerate to separate list into a series of years and indexes
-# if mission year is before 2000, index and year are output into separate arrays
+# if mission year is before 2000, index is output into year_index_list[]
 # for use in later outputs
 for i, v in enumerate(mission_years):
     if v < 2000:
         year_index_list.append(i)
-        mission_years_filtered.append(v)
 
 #prints the mission names matching the values and enumerated index in 
-# year_index_list, prints from mission_names[] and mission_years_filtered[]
-# I added years to fill it out a bit
+# year_index_list, prints from mission_names[] and mission_years[]
+# I added years to fill out the presentation a bit
 print("The names of missions launched before the year 2000 are:")
-for i, v in enumerate(year_index_list):
-    print("- " + mission_names[v] + ", " + str(mission_years_filtered[i]))
+for i in year_index_list:
+    print("- " + mission_names[i] + ", " + str(mission_years[i]))
 
 print("\n***END PROGRAM***\n")
